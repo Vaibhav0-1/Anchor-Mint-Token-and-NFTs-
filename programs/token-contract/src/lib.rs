54988b9ca419a242df35b74c71d5d3c836b19401
Vaibhav <vaibhav.sharma3100@gmail.com>
2024-07-11 00:53:06 +0530

    Transfer To Instruction

--- programs/token-contract/src/lib.rs
+++ programs/token-contract/src/lib.rs
@@ -25,25 +25,27 @@ pub mod token_contract {
         
         Ok(())
     } 
+    
+    pub fn transfer_token(ctx: Context<TransferToken>) -> Result<()> {
+        // Create the Transfer struct for our context
+        let transfer_instruction = Transfer{
+            from: ctx.accounts.from.to_account_info(),
+            to: ctx.accounts.to.to_account_info(),
+            authority: ctx.accounts.from_authority.to_account_info(),
+        };
+        
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        // Create the Context for our Transfer request
+        let cpi_ctx = CpiContext::new(cpi_program, transfer_instruction);
+
+        // Execute anchor's helper function to transfer tokens
+        anchor_spl::token::transfer(cpi_ctx, 5)?;
+
+        Ok(())
+    }
 }
 
-pub fn transfer_token(ctx: Context<TransferToken>) -> Result<()> {
-    // Create the Transfer struct for our context
-    let transfer_instruction = Transfer{
-        from: ctx.accounts.from.to_account_info(),
-        to: ctx.accounts.to.to_account_info(),
-        authority: ctx.accounts.from_authority.to_account_info(),
-    };
-     
-    let cpi_program = ctx.accounts.token_program.to_account_info();
-    // Create the Context for our Transfer request
-    let cpi_ctx = CpiContext::new(cpi_program, transfer_instruction);
 
-    // Execute anchor's helper function to transfer tokens
-    anchor_spl::token::transfer(cpi_ctx, 5)?;
-
-    Ok(())
-}
 
 #[derive(Accounts)]
 pub struct MintToken<'info> {

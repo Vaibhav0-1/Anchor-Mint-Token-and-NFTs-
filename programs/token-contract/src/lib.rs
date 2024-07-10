b665492dbe7aef1eab73060683954947f660494c
Vaibhav <vaibhav.sharma3100@gmail.com>
2024-07-10 23:00:47 +0530

    Transfer To Instruction

--- programs/token-contract/src/lib.rs
+++ programs/token-contract/src/lib.rs
@@ -45,9 +45,6 @@ pub fn transfer_token(ctx: Context<TransferToken>) -> Result<()> {
     Ok(())
 }
 
-
-
-#[derive(Accounts)]
 #[derive(Accounts)]
 pub struct MintToken<'info> {
     /// CHECK: This is the token that we want to mint
@@ -60,4 +57,17 @@ pub struct MintToken<'info> {
     /// CHECK: the authority of the mint account
     #[account(mut)]
     pub authority: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct TransferToken<'info> {
+    pub token_program: Program<'info, Token>,
+    /// CHECK: The associated token account that we are transferring the token from
+    #[account(mut)]
+    pub from: UncheckedAccount<'info>,
+    /// CHECK: The associated token account that we are transferring the token to
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+    // the authority of the from account 
+    pub from_authority: Signer<'info>,
 }
